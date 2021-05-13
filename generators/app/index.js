c8f7ff4e3d17bbf757f342ff433d4edcf5e23df6
chest <630601642@qq.com>
2021-05-13 12:05:48 +0800

    init

---
+++ generators/app/index.js
@@ -0,0 +1,59 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+    prompting() {
+        return this.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'this is a vue generator',
+                default: 'no name'
+            },
+            {
+                type: 'input',
+                name: 'demokey',
+                message: 'this is a vue demokey',
+                default: 'no demokey'
+            }
+        ]).then(answers => this.answers = answers)
+    }
+
+
+    writing() {
+        const templates = [
+            '.browserslistrc',
+            '.editorconfig',
+            '.env.development',
+            '.env.production',
+            '.eslintrc.js',
+            '.gitignore',
+            'babel.config.js',
+            'package.json',
+            'postcss.config.js',
+            'README.md',
+            'public/favicon.ico',
+            'public/index.html',
+            'src/App.vue',
+            'src/main.js',
+            'src/router.js',
+            'src/assets/logo.png',
+            'src/components/HelloWorld.vue',
+            'src/store/actions.js',
+            'src/store/getters.js',
+            'src/store/index.js',
+            'src/store/mutations.js',
+            'src/store/state.js',
+            'src/utils/request.js',
+            'src/views/About.vue',
+            'src/views/Home.vue'
+        ]
+
+        templates.forEach(item=>{
+            this.fs.copyTpl(
+                this.templatePath(item),
+                this.destinationPath(item),
+                this.answers
+            )
+        })
+    }
+}
